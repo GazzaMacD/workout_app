cb694f53b793b88377e056d2fdd2ec6680c7e09b
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-04-27 21:20:17 +0200

    Add server to main

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"github.com/GazzaMacD/workout_app/internal/app"
+	"net/http"
+	"time"
+)
+
+func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+	app.Logger.Println("We are running our app.")
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
