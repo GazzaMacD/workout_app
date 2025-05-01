87b2115c309e271bd6e3cc8ed8668b827cd6b2f7
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-05-01 20:23:18 +0200

    Setup workout handler struct with constructor and simple get handler

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -1,1 +1,34 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type WorkoutHandler struct{}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutID := chi.URLParam(r, "id")
+	if paramsWorkoutID == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// convert string id to base 10, 64 bit int
+	workoutID, err := strconv.ParseInt(paramsWorkoutID, 10, 64)
+	if err != nil {
+		//TODO: - change this at a later stage
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "This is the id of the workout: %d\n", workoutID)
+
+}
