03a678a1bb88be0ce837577be7a14f9d9e1ee627
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-05-08 13:09:47 +0200

    Set up migrations

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/GazzaMacD/workout_app/internal/api"
 	"github.com/GazzaMacD/workout_app/internal/store"
+	"github.com/GazzaMacD/workout_app/migrations"
 )
 
 type Application struct {
@@ -22,6 +23,11 @@ func NewApplication() (*Application, error) {
 	if err != nil {
 		return nil, err
 	}
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	//NOTE: Stores go here
--- internal/store/database.go
+++ internal/store/database.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
+	"io/fs"
 )
 
 func Open() (*sql.DB, error) {
@@ -19,4 +21,24 @@ func Open() (*sql.DB, error) {
 
 	fmt.Println("Connected to Database...")
 	return db, nil
+}
+
+func MigrateFS(db *sql.DB, migrationsFS fs.FS, dir string) error {
+	goose.SetBaseFS(migrationsFS)
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+	return Migrate(db, dir)
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("goose up: %w", err)
+	}
+	return nil
 }
